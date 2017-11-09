03e93b8df517fe4295d066007d58b24d6586f176
Tik00 <33512430+Tik00@users.noreply.github.com>
2017-11-09 12:50:07 +0300

    Create 1_Untitled.sol

---
+++ 1_Untitled.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.13;
+
+contract HelloWorld {   //имя контракта
+                                                        //код контракта
+    function getData() constant returns (string) {  
+        return "Hello, World!";
+    }
+}
